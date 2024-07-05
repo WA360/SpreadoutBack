bb6c8352114002273785d6f3d0d35df2b4d5ab0e
ParkJungHwan <pjh0740@naver.com>
2024-07-05 22:05:36 +0900

    쿠키 테스트

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 
 function checkAuth(req, res, next) {
   const token = req.cookies.token;
+  console.log(token);
   if (!token) {
     return res.status(401).send("인증 토큰이 없습니다.");
   }
