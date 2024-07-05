c56f1d2bb48b2cf36899cc91d9b1108ee9f5ac2e
ParkJungHwan <pjh0740@naver.com>
2024-07-05 20:29:44 +0900

    랭체인 부분 임시 삭제

--- ai/langchan.js
+++ ai/langchan.js
@@ -1,31 +1,30 @@
-require("dotenv").config();
-const { Bedrock } = require("@langchain/community/llms/bedrock");
-// Or, from web environments:
-// import { Bedrock } from "@langchain/community/llms/bedrock/web";
+// require("dotenv").config();
+// const { Bedrock } = require("@langchain/community/llms/bedrock");
+// // Or, from web environments:
+// // import { Bedrock } from "@langchain/community/llms/bedrock/web";
 
-// If no credentials are provided, the default credentials from
-// @aws-sdk/credential-provider-node will be used.
+// // If no credentials are provided, the default credentials from
+// // @aws-sdk/credential-provider-node will be used.
 
-let test = async () => {
-  const model = new Bedrock({
-    model: "meta.llama3-8b-instruct-v1:0", // You can also do e.g. "anthropic.claude-v2"
-    region: "us-east-1",
-    // endpointUrl: "custom.amazonaws.com",
-    credentials: {
-      accessKeyId: process.env.BEDROCK_ACCESS_KEY,
-      secretAccessKey: process.env.BEDROCK_SECRET_ACCESS_KEY,
-    },
-    // modelKwargs: {},
-  });
-  const res = await model.invoke("Tell me a joke");
-  console.log(res);
-};
+// let test = async () => {
+//   const model = new Bedrock({
+//     model: "meta.llama3-8b-instruct-v1:0", // You can also do e.g. "anthropic.claude-v2"
+//     region: "us-east-1",
+//     // endpointUrl: "custom.amazonaws.com",
+//     credentials: {
+//       accessKeyId: process.env.BEDROCK_ACCESS_KEY,
+//       secretAccessKey: process.env.BEDROCK_SECRET_ACCESS_KEY,
+//     },
+//     // modelKwargs: {},
+//   });
+//   const res = await model.invoke("Tell me a joke");
+//   console.log(res);
+// };
 
-test();
-/*
+// test();
+// /*
 
+//   Why was the math book unhappy?
 
-  Why was the math book unhappy?
-
-  Because it had too many problems!
-*/
+//   Because it had too many problems!
+// */
